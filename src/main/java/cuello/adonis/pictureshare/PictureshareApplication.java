e1bd5320951996b376dd43c836ebd5afb5a782d0
Adonis-Cuello <adonisac2002@gmail.com>
2022-01-09 20:00:31 -0500

    first checkin

---
+++ src/main/java/cuello/adonis/pictureshare/PictureshareApplication.java
@@ -0,0 +1,13 @@
+package cuello.adonis.pictureshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cuello/adonis/pictureshare/PictureshareApplicationTests.java
@@ -0,0 +1,13 @@
+package cuello.adonis.pictureshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
